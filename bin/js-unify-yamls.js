2b2eae1a7aef632d18670540e12c072abca037d9
Tomoya Yamaji <yamaji@fusic.co.jp>
2021-10-29 16:08:55 +0900

    feat: unify yamls 1.0.0

--- bin/js-unify-yamls.js
+++ bin/js-unify-yamls.js
@@ -1,9 +1,36 @@
 #!/usr/bin/env node
-'use strict'
-const program = require("commander");
+import fs from "fs";
+import jsYaml from "js-yaml";
+import deepmerge from "deepmerge";
+import { Command } from 'commander';
 
-program.option("-t, --test <test>", "test option")
+'use strict'
+
+const program = new Command();
+
+program.requiredOption('-t, --target <target directory>', 'target directory must be entered.')
+       .requiredOption('-o --output <output name>', 'output name must be entered.')
        .parse(process.argv);
 
 const options = program.opts();
-console.log(options)
+let ret = {}
+
+const pathStack = [options.target]
+while (pathStack.length > 0) {
+  const path = pathStack.pop();
+  const dirents = fs.readdirSync(path, { withFileTypes: true });
+
+  dirents.forEach((dirent) => {
+    const newPath = `${path}/${dirent.name}`;
+    if (dirent.isDirectory()) {
+      pathStack.push(newPath);
+    } else {
+      const yamlText = fs.readFileSync(newPath, "utf8");
+      const yamlObj = jsYaml.load(yamlText);
+      ret = deepmerge(ret, yamlObj || {});
+    }
+  } );
+}
+
+const fileContent = `export default ${JSON.stringify(ret)}`;
+fs.writeFileSync(options.output, fileContent);
