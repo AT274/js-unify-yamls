2f33e41f3eff2c851aef8042d41f76dd80aea743
Tomoya Yamaji <yamaji@fusic.co.jp>
2021-10-29 14:20:05 +0900

    feat: my first CLI :)

--- bin/js-unify-yamls.js
+++ bin/js-unify-yamls.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log('hello :)');
+'use strict'
+const program = require("commander");
+
+program.option("-t, --test <test>", "test option")
+       .parse(process.argv);
+
+const options = program.opts();
+console.log(options)
